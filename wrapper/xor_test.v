19e8984f483cb428599b0368fb884bc2772b7465
Adityapatil11 <adityappatil11@gmail.com>
2023-06-02 06:45:43 +0530

    xor2

---
+++ wrapper/xor_test.v
@@ -0,0 +1,16 @@
+module xor_test(
+    input wire a,
+    input wire b,
+    output wire y
+);
+
+dut dut_test(
+    a(a),
+    b(b),
+    y(y)
+)
+initial begin
+    $dumpfile("waves.vcd");
+    $dumpvars;
+end
+endmodule
